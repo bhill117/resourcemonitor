64350b68abe4d148e1370a7808562fae0c4a1438
Brandon Hill <brandon.hill1025@gmail.com>
2017-10-13 13:22:34 -0600

    added performance counters

--- ResourceMonitor/Program.cs
+++ ResourceMonitor/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Threading;
+using System.Speech.Synthesis;
 
 namespace ResourceMonitor
 {
@@ -10,6 +13,27 @@ namespace ResourceMonitor
     {
         static void Main(string[] args)
         {
+            //Greeting
+            SpeechSynthesizer synth = new SpeechSynthesizer();
+            synth.Speak("Welcome to Resource Monitor");
+
+            #region Performance Counters
+
+            //CPU Usage
+            PerformanceCounter perfCpuCount = new PerformanceCounter("Processor Information", "% Processor Time", "_Total");
+            perfCpuCount.NextValue();
+
+            //Memory Usage
+            PerformanceCounter perfMemCount = new PerformanceCounter("Memory", "Available MB");
+            perfMemCount.NextValue();
+
+            //Running Time
+            PerformanceCounter perfUpTimeCount = new PerformanceCounter("System", "System Up Time");
+            perfUpTimeCount.NextValue();
+            #endregion
+
+
+
         }
     }
 }
