f3bc64e4bf0a4781c6e2e0b9b7ca57dfd1041dd2
Brandon Hill <brandon.hill1025@gmail.com>
2017-10-13 12:54:56 -0600

    initial commit

---
+++ ResourceMonitor/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResourceMonitor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
